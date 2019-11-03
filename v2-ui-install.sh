9f2d3d4d2e6d7c8936e207ba05541a539f2309ca
thokehbrt <52646385+RomanHrbr@users.noreply.github.com>
2019-11-02 18:25:29 -0600

    Update v2-ui-install.sh

--- v2-ui-install.sh
+++ v2-ui-install.sh
@@ -93,7 +93,7 @@ install_v2-ui() {
     sudo mkdir /usr/local/v2-ui
     #last_version=$(curl -Ls "https://api.github.com/repos/sprov065/v2-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     #echo -e "Se detectó la última versión de v2ray-Panel：${last_version}，Comience la instalación"
-    wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz https://www.dropbox.com/s/zvp2ev2ap6m999a/v2-ui-linux.tar.gz
+    wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz https://www.dropbox.com/s/zvp2ev2ap6m999a/v2-ui-linux.tar.gz?dl=0
     if [[ $? -ne 0 ]]; then
         echo -e "${red}La descarga de v2-ui falló, asegúrese de que su servidor pueda descargar archivos Dropbox, si falla la instalación múltiple, consulte el tutorial de instalación manual${plain}"
         exit 1
