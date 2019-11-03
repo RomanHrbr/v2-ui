55f33e2f2971ce8b8bb9482054c270ed540e006f
thokehbrt <52646385+RomanHrbr@users.noreply.github.com>
2019-11-02 18:05:58 -0600

    Update v2-ui-install.sh

--- v2-ui-install.sh
+++ v2-ui-install.sh
@@ -92,10 +92,11 @@ install_v2-ui() {
     if [[ -e /usr/local/v2-ui/ ]]; then
         rm /usr/local/v2-ui/ -rf
     fi
-    if [[ -e /usr/local/v2-ui/ ]]; then
-        mkdir /usr/local/v2-ui/ -rf
+    if [[ -e /etc/ger-frm ]]; then
+    echo "" 
+    else
+    sudo mkdir /usr/local/v2-ui
     fi
-    #sudo mkdir /usr/local/v2-ui
     #last_version=$(curl -Ls "https://api.github.com/repos/sprov065/v2-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     #echo -e "Se detectó la última versión de v2ray-Panel：${last_version}，Comience la instalación"
     wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz https://www.dropbox.com/s/ghsyokoylpkhlvl/v2-ui-linux.tar.gz
